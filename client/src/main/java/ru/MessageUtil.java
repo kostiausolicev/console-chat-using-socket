84f554de0b0a360e27a6396a95a631e5e54c4f7d
kostiausolicev <kostiausolicev@gmail.com>
2023-07-18 01:22:59 +0500

    add user names

---
+++ client/src/main/java/ru/MessageUtil.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class MessageUtil {
+    private String name;
+    private String message;
+
+    public static String ser(MessageUtil util) {
+        var s = new StringBuilder();
+        s.append("name=").append(util.getName()).append(";");
+        s.append("message=").append(util.getMessage());
+
+        return s.toString();
+    }
+
+    public static MessageUtil deser(String s) {
+        var util = new MessageUtil();
+        var arr = s.split(";");
+        for (var arg : arr) {
+            var params = arg.split("=");
+            if (params[0].equals("name")) util.setName(params[1]);
+            else if (params[0].equals("message")) util.setMessage(params[1]);
+        }
+        return util;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
